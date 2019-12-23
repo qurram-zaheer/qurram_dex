f68ca7c0269da04c902051da7cf823ab11b9c9b2
Qurram Zaheer <qurramzaheer@Qurrams-MacBook-Air.local>
2019-12-24 01:59:34 +0530

    card transitions

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import spinner from '../pokemon/spinner.gif'
 
+const Sprite = styled.img`
+    width: 5em;
+    height: 5em;
+    display: none;
+`
 const Card = styled.div`
-
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    &:hover{
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25),0 10px 10px rgba(0,0,0,0.22)
+    }
+    -moz-user-select: none;
+    -website-user-select: none;
+    -user-select: none;
+    -o-user-select: none;
 
 `;
 
@@ -33,12 +46,28 @@ export default class PokemonCard extends Component {
             
             <div className = 'col-md-2 col-sm-6 mb-5'>
                 
-                <div className = 'card'>
-                    <h5 className = 'card-header'>{this.state.pokemonIndex}</h5>
+                <Card className = 'card'>
+                    <h5 className = 'card-header'>#{this.state.pokemonIndex}</h5>
                     {this.state.imageLoading ? (
-                        <img src = {spinner}  className = 'card-img-top mx-auto mt-5'></img>
+                        <img src = {spinner}  
+                        className = 'card-img-top mx-auto mt-5'
+                        style = {{width: "5em",height:"5em"}}
+                        ></img>
                     ):null}
-                    <img onLoad = {() => this.setState({imageLoading: false})} onError = {() => this.setState({error:true})} style = {this.state.error ? {display: "none"}:null,this.state.imageLoading ? null:{display: "block"},{width: "120px", height: "120px", backgroundPosition: "50% 50%", position:"relative",float:"left",backgroundSize:"cover",backgroundRepeat:"no-repeat"}} className = 'card-img-top mx-auto mt-2'  src = {this.state.img}></img>
+                    {/* <img onLoad = {() => this.setState({imageLoading: false})} onError = {() => this.setState({error:true})} style = {this.state.error ? {display: "none"}:null,this.state.imageLoading ? null:{display: "block"},{width: "120px", height: "120px", backgroundPosition: "50% 50%", position:"relative",float:"left",backgroundSize:"cover",backgroundRepeat:"no-repeat"}} className = 'card-img-top mx-auto mt-2'  src = {this.state.img}></img> */}
+                    <Sprite
+                    className="card-img-top rounded mx-auto mt-2"
+                    src={this.state.img}
+                    onLoad={() => this.setState({ imageLoading: false })}
+                    onError={() => this.setState({ error: true })}
+                    style={
+                    this.state.error
+                        ? { display: 'none' }
+                        : this.state.imageLoading
+                        ? null
+                        : { display: 'block' }
+              }
+            />
                     {
                     this.state.error ? (<h6 className = 'mx-auto'>
                         <span className = 'badge badge-danger mt-2'>Too many requests</span>
@@ -47,7 +76,7 @@ export default class PokemonCard extends Component {
                     <div className = 'card-body mx-auto'>
                         <h6 className= 'card-title '>{this.state.name.toLowerCase().split(" ").map(letter => letter.charAt(0).toUpperCase() + letter.substring(1)).join(" ")}</h6>
                     </div>
-                </div>
+                </Card>
             </div>
         )
     }
