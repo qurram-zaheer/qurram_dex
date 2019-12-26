a4459281ec00766bb6aac0f7c68332112f920c88
Qurram Zaheer <qurramzaheer@Qurrams-MacBook-Air.local>
2019-12-26 19:43:26 +0530

    Fixed link to Git repo

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -9,7 +9,7 @@ export default class Navbar extends Component {
             <div>
                 <nav className = "navbar fixed-top nav-shadow" >
                     <b><a className= ' mx-2 navbar-brand col-sm-3 col-md-2 align-items-center' >Qurram's pokedex</a></b>
-                    <a href = 'https://github.com/qurram-zaheer/qurram_dex'>
+                    <a href = 'https://github.com/qurram-zaheer/qurram_dex' target = "_blank">
                     <span className="navbar-text def-font">
                     
                         <span className="badge badge-pill badge-secondary git">
